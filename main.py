70aab78deaeea8dab2224406121e75f4f1518b8e
felipe bastos <felipebastosweb@gmail.com>
2018-03-31 03:25:19 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from weppy import App, request, abort, redirect, session
+from weppy.tools import service
+from weppy.sessions import SessionFSManager
+
+app = App(__name__)
+app.pipeline = [SessionFSManager()]
+
+# Load database connection
+import services
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
