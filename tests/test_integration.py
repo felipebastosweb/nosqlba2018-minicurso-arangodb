e0806370d0da5cfd98a15949950daa29e1ecac1d
felipe bastos <felipebastosweb@gmail.com>
2020-07-23 11:01:23 -0300

    Create test_integration.py

---
+++ tests/test_integration.py
@@ -0,0 +1,44 @@
+import pytest
+
+from ..schema import *
+
+### ArangoDB Integration
+## Account Collection
+
+def test_account_signup():
+    assert False
+
+def test_account_exists():
+    assert False
+
+def test_account_find():
+    assert False
+
+def test_account_signin():
+    assert False
+
+def test_account_update_email():
+    assert False
+
+def test_account_update_password():
+    assert False
+
+def test_account_arquive():
+    assert False
+
+def test_account_delete():
+    assert False
+
+## Event Collection
+
+## Subscription Collection
+
+## Blog Collection
+
+## Post Collection
+
+## Comment Collection
+
+
+
+# Reference - https://realpython.com/pytest-python-testing/
